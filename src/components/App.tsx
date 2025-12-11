711af76d47732b200daa17e145374c26ab89fc58
Adam Aaronson <adamaaronson@gmail.com>
2025-12-11 18:40:46 -0500

    Remove unused import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { type MemoryQueue, type Card, reshuffle, isNew } from "../types/memory";
 import { shuffle } from "lodash";
 import deck from "../decks/json/world_capitals.json";
