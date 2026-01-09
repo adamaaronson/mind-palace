31346fbf2a500d3505aa07f1857fd7200e7c5dd1
Adam Aaronson <adamaaronson@gmail.com>
2026-01-09 09:55:55 -0500

    Tweak modal overflow

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -27,22 +27,24 @@ export function Modal(props: ModalProps) {
             onClick={onClose}
           ></div>
           <div
-            className={`bg-light border-standard p-4 px-8 min-w-0 z-52 relative inset-shadow-scroll ${className}`}
+            className={`bg-light border-standard min-w-0 z-52 relative inset-shadow-scroll m-4 ${className}`}
           >
             <div className="absolute top-0 w-[calc(100%+48px)] h-2 -left-6 bg-text-dark -translate-y-5 rounded-full"></div>
             <div className="absolute top-0 w-[calc(100%+24px)] h-4 -left-3 bg-text-dark -translate-y-6"></div>
             <div className="absolute top-0 w-[calc(100%+16px)] h-8 border-standard -left-2 bg-light -translate-y-full inset-shadow-glossy"></div>
-            <motion.div
-              key={Math.random()}
-              initial={{ height: 0 }}
-              animate={{ height: "auto" }}
-              className="overflow-hidden"
-            >
-              <div className="font-bold text-3xl mb-4 font-classical text-center">
-                {title}
-              </div>
-              {children}
-            </motion.div>
+            <div className="p-4 px-8 overflow-scroll max-h-[calc(100vh-64px)]">
+              <motion.div
+                key={Math.random()}
+                initial={{ height: 0 }}
+                animate={{ height: "auto" }}
+                className="overflow-hidden"
+              >
+                <div className="font-bold text-3xl mb-4 font-classical text-center">
+                  {title}
+                </div>
+                {children}
+              </motion.div>
+            </div>
             <div className="absolute top-full w-[calc(100%+48px)] h-2 -left-6 bg-text-dark translate-y-3 rounded-full"></div>
             <div className="absolute top-full w-[calc(100%+24px)] h-4 -left-3 bg-text-dark translate-y-2"></div>
             <div className="absolute top-full w-[calc(100%+16px)] h-8 border-standard -left-2 bg-light inset-shadow-glossy"></div>
