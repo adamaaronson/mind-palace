b948bf53b9fbbf5d921c3295ffa1e6bbd68ddc1c
Adam Aaronson <adamaaronson@gmail.com>
2026-01-19 21:59:02 -0500

    Round column capitals

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -5,15 +5,15 @@ function Flute() {
 export default function Column() {
   return (
     <div className="flex-none md:block hidden h-full relative mr-1 ml-0.5 z-100">
-      <div className="bg-light border-standard border-t-0! w-[calc(100%+2px)] h-2 absolute top-0 left-0" />
+      <div className="bg-light rounded-full border-standard w-full h-2.5 -translate-y-0.5 absolute top-0 left-0" />
       <div className="flex h-full">
-        <div className="w-2 h-full"></div>
+        <div className="w-1.5 h-full"></div>
         {Array.from({ length: 7 }, (_, index) => (
           <Flute key={index} />
         ))}
         <div className="border-l-standard w-2 h-full"></div>
       </div>
-      <div className="bg-light border-standard border-b-0! w-[calc(100%+2px)] h-2 absolute top-full left-0 -translate-y-full" />
+      <div className="bg-light rounded-full border-standard w-full h-2.5 absolute top-full left-0 translate-y-[calc(-100%+2px)]" />
     </div>
   );
 }
