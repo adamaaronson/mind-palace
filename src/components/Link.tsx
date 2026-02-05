13dc1ea60570c1e7018d0a6d6c9336f7a52b6a0d
Adam Aaronson <adamaaronson@gmail.com>
2026-02-05 00:15:50 -0500

    Tweak link line breaks

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -14,7 +14,7 @@ export default function Link(props: LinkProps) {
       target="_blank"
       rel="noopener noreferrer"
       onClick={onClick}
-      className="font-bold text-text-dark inline-block hover:text-text-light transition-colors"
+      className="font-bold text-text-dark hover:text-text-light transition-colors"
       style={{
         backgroundImage: "linear-gradient(#9c7e4e 0%, #9c7e4e 100%)",
         backgroundPosition: "0 1.15em",
@@ -22,18 +22,21 @@ export default function Link(props: LinkProps) {
         backgroundSize: "6px 1px",
       }}
     >
-      {children}
-      {wikipedia ? (
-        <img
-          src="wikipedia.svg"
-          className="inline-block w-[1em] align-middle ml-1 mb-[0.1em]"
-        ></img>
-      ) : (
-        <img
-          src="external.svg"
-          className="inline-block w-[0.6em] align-middle ml-1 mb-[0.15em]"
-        ></img>
-      )}
+      {children.split(" ").slice(0, -1).join(" ")}{" "}
+      <span className="whitespace-nowrap">
+        {children.split(" ").at(-1)}
+        {wikipedia ? (
+          <img
+            src="wikipedia.svg"
+            className="inline-block w-[1em] align-middle ml-1 mb-[0.1em]"
+          ></img>
+        ) : (
+          <img
+            src="external.svg"
+            className="inline-block w-[0.6em] align-middle ml-1 mb-[0.15em]"
+          ></img>
+        )}
+      </span>
     </a>
   ) : (
     <span className="font-bold text-text-dark">{children}</span>
