9d8631a1bbae410cb79c00f66f8f80f06606ecd7
Adam Aaronson <adamaaronson@gmail.com>
2026-02-10 21:05:43 -0500

    Increase footer letter spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-light border-t-standard flex-none z-100 text-center font-classical p-0.5 font-bold text-text-light">
+    <footer className="bg-light border-t-standard flex-none z-100 text-center font-classical p-0.5 font-bold text-text-light tracking-widest">
       <span className="text-text-light md:text-xl sm:hidden">
         v. Pre-Alpha <span className="text-text-dark"> • </span>
       </span>
